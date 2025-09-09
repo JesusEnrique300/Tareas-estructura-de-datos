4ebe14a773cbcada641217c4345e6810b96d699c
Jesus Enrique <jenrique34578@gmail.com>
2025-09-09 01:17:54 -0700

    Ejercicio de arreglos
    Definir un arreglo tridimensional y recorrerlo

---
+++ C#/Arreglos4.cs
@@ -0,0 +1,23 @@
+using System;
+
+class Program {
+    static void Main() {
+        int[,,] arr = {
+            { {1, 2}, {3, 4} },
+            { {5, 6}, {7, 8} }
+        };
+
+        for(int i = 0; i < 2; i++) {
+            for(int j = 0; j < 2; j++) {
+                for(int k = 0; k < 2; k++) {
+                    Console.WriteLine($"arr[{i},{j},{k}] = {arr[i,j,k]}");
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
