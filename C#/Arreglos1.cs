c4e725d68aa34def226d6d258f085b834209ba1b
Jesus Enrique <jenrique34578@gmail.com>
2025-09-09 00:32:35 -0700

    Ejercicio arreglos
    Ejercicios que sirven para recorrer un arreglo e imprimir cada uno de los valores

---
+++ C#/Arreglos1.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program {
+    static void Main() {
+        int[] arr = {10, 20, 30, 40, 50};
+
+        foreach(int num in arr) {
+            Console.WriteLine(num);
+        }
+    }
+}
+
